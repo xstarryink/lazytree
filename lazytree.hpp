d5d4add2e029bde4aaffbd350937ac3ef42cde98
reverSilly <75513686+reverSilly@users.noreply.github.com>
2022-02-03 23:36:38 +0800

    Create lazytree.hpp
    a new implementation for lazytree which provides both set and map.

---
+++ lazytree.hpp
@@ -0,0 +1,139 @@
+#pragma once
+#include<type_traits>
+#include<bits/unique_ptr.h>
+#include<bits/move.h>
+#include<bit>
+#include<limits>
+namespace{struct EmptyClass{};}
+template<typename key_t,typename value_t=EmptyClass> requires std::is_integral_v<key_t>
+class LazyTree
+{
+public:
+	struct Node;
+	using ptrNode=std::unique_ptr<Node>;
+	using ukey_t=std::make_unsigned_t<key_t>;
+	static inline int highbit(key_t value)
+	{
+		return std::numeric_limits<key_t>::digits-std::__countl_zero(ukey_t(value))-1;
+	}
+	struct Node:value_t
+	{
+		key_t key;
+		ptrNode son[2];
+	};
+	static constexpr key_t invalidKey{};
+	
+	ptrNode root;
+	
+	LazyTree()noexcept=default;
+	~LazyTree()=default;
+	void clear()
+	{
+		root.reset(nullptr);
+	}
+	value_t& insert(key_t key)
+	{
+		return *HalfwayInsert(++key,key_t(1)<<highbit(key)>>1,&root);
+	}
+	value_t& at(key_t key)
+	{
+		++key;
+		ptrNode *cur_node=&root;
+		Node *emptyNode{nullptr}; 
+		key_t mask=1<<highbit(key)>>1;
+		for(;mask&&(*cur_node)&&((*cur_node)->key)!=key;mask>>=1)
+		{
+			if((*cur_node)->key==invalidKey)
+			{
+				emptyNode=cur_node->get();
+			}
+			cur_node=(*cur_node)->son+static_cast<bool>(key&mask);
+		}
+		if((*cur_node)&&((*cur_node)->key==key))
+		{
+//			found
+			if(emptyNode)
+			{
+				emptyNode->key=key;
+				dynamic_cast<value_t&>(*emptyNode)=std::move(dynamic_cast<value_t&>(**cur_node));
+				eraseNode(cur_node);
+				return dynamic_cast<value_t&>(*emptyNode);
+			}
+			return **cur_node;
+		}
+		else
+		{
+			throw "key quiried not found";
+		}
+	}
+	const value_t& at(key_t key) const
+	{
+		++key;
+		ptrNode const *cur_node=&root;
+		key_t mask=1<<highbit(key)>>1;
+		for(;mask&&(*cur_node)&&((*cur_node)->key)!=key;mask>>=1)
+			cur_node=(*cur_node)->son+static_cast<bool>(key&mask);
+		if((*cur_node)&&((*cur_node)->key==key))
+			return dynamic_cast<value_t&>(**cur_node);
+		else
+			throw "key not found";
+	}
+	bool has(key_t key) const noexcept
+	{
+		try
+		{
+			at(key);
+			return true;
+		}
+		catch(const char*)
+		{
+			return false;
+		}
+	}
+	void erase(key_t key)
+	{
+		++key;
+		ptrNode *cur_node=&root;
+		key_t mask=1<<highbit(key)>>1;
+		for(;mask&&(*cur_node)&&((*cur_node)->key)!=key;mask>>=1)
+			cur_node=(*cur_node)->son+static_cast<bool>(key&mask);
+		if((*cur_node)&&(*cur_node)->key==key)
+			eraseNode(cur_node);
+		else
+			throw "key not found";
+	}
+private:
+	Node *HalfwayInsert(const key_t key,key_t mask,ptrNode *cur_node)
+	{
+		for(;mask&&(*cur_node)&&((*cur_node)->key!=invalidKey);mask>>=1)
+		{
+			cur_node=(*cur_node)->son+static_cast<bool>(key&mask);
+		}
+		if((!(*cur_node))||(((*cur_node)->key)==invalidKey))
+		{
+//			found an empty node on the way home
+//			get lazy
+			cur_node->reset(new Node{.key=key});
+			return cur_node->get();
+		}
+		else
+		{
+//			this is home and the node is occupied
+//			expel the occupier
+			dynamic_cast<value_t&>(*HalfwayInsert((*cur_node)->key,1<<(highbit((*cur_node)->key)-highbit(key)-1),cur_node))=std::move(dynamic_cast<value_t&>(**cur_node));
+			(*cur_node)->key=key;
+			return cur_node->get();
+		}
+	}
+	void eraseNode(ptrNode *node)
+	{
+		if(!(*node)->son[0]&&!(*node)->son[1])
+		{
+			node->reset();
+		}
+		else
+		{
+			(*node)->key=invalidKey;
+		}
+	}
+};
