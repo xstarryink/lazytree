8efaae485b9464c5e2940ef81c3902457236f5fc
reverSilly <75513686+reverSilly@users.noreply.github.com>
2022-02-04 15:35:27 +0800

    fix some bugs
    1. fix incorrect mask for key '0'.
    2. fix the action in expelling node.

--- lazytree.hpp
+++ lazytree.hpp
@@ -8,7 +8,6 @@ namespace{struct EmptyClass{};}
 template<typename key_t,typename value_t=EmptyClass> requires std::is_integral_v<key_t>
 class LazyTree
 {
-public:
 	struct Node;
 	using ptrNode=std::unique_ptr<Node>;
 	using ukey_t=std::make_unsigned_t<key_t>;
@@ -20,11 +19,53 @@ public:
 	{
 		key_t key;
 		ptrNode son[2];
+		Node(key_t key)
+		:value_t(),key{key},son{}{}
 	};
 	static constexpr key_t invalidKey{};
-	
 	ptrNode root;
-	
+	Node *HalfwayInsert(const key_t key,key_t mask,ptrNode *cur_node)
+	{
+		for(;mask&&(*cur_node)&&((*cur_node)->key!=invalidKey)&&((*cur_node)->key!=key);mask>>=1)
+		{
+			cur_node=(*cur_node)->son+static_cast<bool>(key&mask);
+		}
+		if((!(*cur_node))||(((*cur_node)->key)==invalidKey))
+		{
+//			found an empty node on the way home
+//			get lazy
+			cur_node->reset(new Node(key));
+			return cur_node->get();
+		}
+		else
+		{
+//			this is home and the node is occupied
+//			expel the occupier
+			if((*cur_node)->key!=key)
+			{
+				key_t occupier=(*cur_node)->key;
+				(*cur_node)->key=key;
+				dynamic_cast<value_t&>(*HalfwayInsert(occupier,key_t(1)<<highbit(occupier)>>highbit(key)>>1,cur_node))=std::move(dynamic_cast<value_t&>(**cur_node));
+				return cur_node->get();
+			}
+			else
+			{
+				throw "Key already exist";
+			}
+		}
+	}
+	void eraseNode(ptrNode *node)
+	{
+		if(((*node)->son[0])||((*node)->son[1]))
+		{
+			(*node)->key=invalidKey;
+		}
+		else
+		{
+			node->reset();
+		}
+	}
+public:
 	LazyTree()noexcept=default;
 	~LazyTree()=default;
 	void clear()
@@ -33,13 +74,14 @@ public:
 	}
 	value_t& insert(key_t key)
 	{
-		return *HalfwayInsert(++key,key_t(1)<<highbit(key)>>1,&root);
+		++key;
+		return *HalfwayInsert(key,key_t(1)<<highbit(key)>>1,&root);
 	}
 	value_t& at(key_t key)
 	{
 		++key;
 		ptrNode *cur_node=&root;
-		Node *emptyNode{nullptr}; 
+		Node *emptyNode{nullptr};
 		key_t mask=1<<highbit(key)>>1;
 		for(;mask&&(*cur_node)&&((*cur_node)->key)!=key;mask>>=1)
 		{
@@ -70,7 +112,7 @@ public:
 	{
 		++key;
 		ptrNode const *cur_node=&root;
-		key_t mask=1<<highbit(key)>>1;
+		key_t mask=key_t(1)<<highbit(key)>>1;
 		for(;mask&&(*cur_node)&&((*cur_node)->key)!=key;mask>>=1)
 			cur_node=(*cur_node)->son+static_cast<bool>(key&mask);
 		if((*cur_node)&&((*cur_node)->key==key))
@@ -90,50 +132,28 @@ public:
 			return false;
 		}
 	}
+	bool count(key_t key) const noexcept
+	{
+		try
+		{
+			at(key);
+			return true;
+		}
+		catch(const char*)
+		{
+			return false;
+		}
+	}
 	void erase(key_t key)
 	{
 		++key;
 		ptrNode *cur_node=&root;
-		key_t mask=1<<highbit(key)>>1;
+		key_t mask=key_t(1)<<highbit(key)>>1;
 		for(;mask&&(*cur_node)&&((*cur_node)->key)!=key;mask>>=1)
 			cur_node=(*cur_node)->son+static_cast<bool>(key&mask);
 		if((*cur_node)&&(*cur_node)->key==key)
 			eraseNode(cur_node);
 		else
 			throw "key not found";
-	}
-private:
-	Node *HalfwayInsert(const key_t key,key_t mask,ptrNode *cur_node)
-	{
-		for(;mask&&(*cur_node)&&((*cur_node)->key!=invalidKey);mask>>=1)
-		{
-			cur_node=(*cur_node)->son+static_cast<bool>(key&mask);
-		}
-		if((!(*cur_node))||(((*cur_node)->key)==invalidKey))
-		{
-//			found an empty node on the way home
-//			get lazy
-			cur_node->reset(new Node{.key=key});
-			return cur_node->get();
-		}
-		else
-		{
-//			this is home and the node is occupied
-//			expel the occupier
-			dynamic_cast<value_t&>(*HalfwayInsert((*cur_node)->key,1<<(highbit((*cur_node)->key)-highbit(key)-1),cur_node))=std::move(dynamic_cast<value_t&>(**cur_node));
-			(*cur_node)->key=key;
-			return cur_node->get();
-		}
-	}
-	void eraseNode(ptrNode *node)
-	{
-		if(!(*node)->son[0]&&!(*node)->son[1])
-		{
-			node->reset();
-		}
-		else
-		{
-			(*node)->key=invalidKey;
-		}
 	}
 };
